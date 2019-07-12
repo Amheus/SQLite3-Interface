423b9cd6e27ce5262691ac2eb8536bcf9270b46a
Sam22122001 <35064657+Sam22122001@users.noreply.github.com>
2019-07-12 16:06:55 +0100

    Create Python.py

---
+++ Python.py
@@ -0,0 +1,17 @@
+import sqlite3
+
+def sqlOutput(query, location):
+    conn = sqlite3.connect(location)
+    cursor = conn.cursor()
+    cursor.execute(query)
+    temp = cursor.fetchall()
+    conn.close()
+    return(temp)
+
+def sqlInput(query, location):
+    conn = sqlite3.connect(location)
+    cursor = conn.cursor()
+    #cursor.execute(query, args)
+    cursor.execute(query)
+    conn.commit()
+    conn.close()
