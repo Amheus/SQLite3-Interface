7629ad35deb4cdebaa8b22390c367c0ede973224
Sam22122001 <35064657+Sam22122001@users.noreply.github.com>
2019-07-12 16:13:25 +0100

    Update Python.py

--- Python.py
+++ Python.py
@@ -1,18 +1,19 @@
-import sqlite3
+### I don't really use Python all that often so this code isn't very well commented.
 
-def sqlOutput(query, location):
-    conn = sqlite3.connect(location)
-    cursor = conn.cursor()
-    cursor.execute(query)
-    temp = cursor.fetchall()
-    conn.close()
-    return(temp)
+import sqlite3 # Imports SQLite, fairly sure that you need to install SQLite aswell for Python.
 
-def sqlInput(query, location):
-    conn = sqlite3.connect(location)
-    cursor = conn.cursor()
-    #cursor.execute(query, args)
-    cursor.execute(query)
+def sqlOutput(query, location): # For getting data out of a database.
+    conn = sqlite3.connect(location) # Creates the connection
+    cursor = conn.cursor() # Creates a cursor, this is like the adapter in .NET (I think!)
+    cursor.execute(query) # Executes the query
+    temp = cursor.fetchall() # Populates the returned data into a variable so that the connection can be closed without loosing the data.
+    conn.close() # Closes the connection so that the database is no longer locked.
+    return(temp) # Returns the data in 'temp'.
+
+def sqlInput(query, location): # For putting data into the database.
+    conn = sqlite3.connect(location) # Creates the connection
+    cursor = conn.cursor() # Creates a cursor, this is like the adapter in .NET (I think!)
+    cursor.execute(query) # Rxecutes the query
     conn.commit()
-    conn.close()
+    conn.close() # Closes the connection so that the database is no longer locked.
     
